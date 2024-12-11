c0c74ac4e8b850a73fe6db7c3ee63cac171abf96
Bart de Lange <bart.de.lange32@gmail.com>
2024-12-11 19:25:05 +0100

    chore: Changing to my own version of responsive-linechart

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -12,28 +12,27 @@ import { LogBox } from 'react-native';
 import { navigationTheme, paperTheme } from './theming';
 
 LogBox.ignoreLogs([
-  'Non-serializable values were found in the navigation state',
+    'Non-serializable values were found in the navigation state',
 ]);
-
 export default function App() {
-  React.useEffect(() => {
-    RNBootSplash.hide({ fade: true });
-  }, []);
+    React.useEffect(() => {
+        RNBootSplash.hide({ fade: true });
+    }, []);
 
-  return (
-    <SafeAreaProvider
-      style={{ flex: 1, backgroundColor: paperTheme.colors.background }}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <PaperProvider theme={paperTheme}>
-          <NavigationContainer theme={navigationTheme}>
-            <AuthProvider>
-              <Preloader>
-                <Router />
-              </Preloader>
-            </AuthProvider>
-          </NavigationContainer>
-        </PaperProvider>
-      </GestureHandlerRootView>
-    </SafeAreaProvider>
-  );
+    return (
+        <SafeAreaProvider
+            style={{ flex: 1, backgroundColor: paperTheme.colors.background }}>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <PaperProvider theme={paperTheme}>
+                    <NavigationContainer theme={navigationTheme}>
+                        <AuthProvider>
+                            <Preloader>
+                                <Router />
+                            </Preloader>
+                        </AuthProvider>
+                    </NavigationContainer>
+                </PaperProvider>
+            </GestureHandlerRootView>
+        </SafeAreaProvider>
+    );
 }
