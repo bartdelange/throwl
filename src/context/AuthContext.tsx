f8fee20212478e1413e9b259958ebc14be818f3a
Bart de Lange <bart.de.lange32@gmail.com>
2025-04-17 22:37:05 +0200

    chore: Fixing firebase deprecated non-modular access

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,72 +1,96 @@
-import auth from '@react-native-firebase/auth';
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+} from '@react-native-firebase/auth';
 import React, {
-  createContext,
-  PropsWithChildren,
-  useEffect,
-  useState,
+    createContext,
+    PropsWithChildren,
+    useEffect,
+    useState,
 } from 'react';
 import { User } from '~/models/user';
 import { UserService } from '~/services/user_service';
 
 interface AuthContext {
-  user?: User;
-  initializing: boolean;
-  login: (email: string, password: string) => Promise<void>;
-  register: (email: string, password: string, name: string) => Promise<void>;
-  logout: () => Promise<void>;
+    user?: User;
+    initializing: boolean;
+    login: (email: string, password: string) => Promise<void>;
+    register: (email: string, password: string, name: string) => Promise<void>;
+    logout: () => Promise<void>;
 }
 
 // @ts-ignore Don't want to implement on creation as it is done below immediately
 export const AuthContext = createContext<AuthContext>({});
 export const AuthProvider: React.FC<PropsWithChildren<any>> = ({
-  children,
+    children,
 }) => {
-  const [user, setUser] = useState<User>();
-  const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState<User>();
+    const [initializing, setInitializing] = useState(true);
 
-  async function onAuthStateChanged(user: any) {
-    if (user?.uid) {
-      setUser(await UserService.getById(user.uid));
+    async function _onAuthStateChanged(user: any) {
+        if (user?.uid) {
+            setUser(await UserService.getById(user.uid));
+        }
+        if (initializing) setInitializing(false);
     }
-    if (initializing) setInitializing(false);
-  }
 
-  useEffect(() => {
-    if (user) {
-      return UserService.listenToUserChanges(user.id, data => {
-        if (JSON.stringify(user) !== JSON.stringify(data)) setUser(data);
-      });
-    }
-  }, [user]);
+    useEffect(() => {
+        if (user) {
+            return UserService.listenToUserChanges(user.id, data => {
+                if (JSON.stringify(user) !== JSON.stringify(data))
+                    setUser(data);
+            });
+        }
+    }, [user]);
 
-  useEffect(() => {
-    return auth().onAuthStateChanged(onAuthStateChanged); // unsubscribe on unmount
-  }, []);
+    useEffect(() => {
+        return onAuthStateChanged(getAuth(), _onAuthStateChanged); // unsubscribe on unmount
+    }, []);
 
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        initializing,
-        login: async (email: string, password: string) => {
-          await auth().signInWithEmailAndPassword(email, password);
-        },
-        register: async (email: string, password: string, name: string) => {
-          const userCredential = await auth().createUserWithEmailAndPassword(
-            email,
-            password
-          );
-          await auth().signInWithEmailAndPassword(email, password);
-          setUser(
-            await UserService.create(userCredential.user.uid, email, name)
-          );
-        },
-        logout: async () => {
-          await auth().signOut();
-          setUser(undefined);
-        },
-      }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                initializing,
+                login: async (email: string, password: string) => {
+                    await signInWithEmailAndPassword(
+                        getAuth(),
+                        email,
+                        password
+                    );
+                },
+                register: async (
+                    email: string,
+                    password: string,
+                    name: string
+                ) => {
+                    const userCredential = await createUserWithEmailAndPassword(
+                        getAuth(),
+                        email,
+                        password
+                    );
+                    await signInWithEmailAndPassword(
+                        getAuth(),
+                        email,
+                        password
+                    );
+                    setUser(
+                        await UserService.create(
+                            userCredential.user.uid,
+                            email,
+                            name
+                        )
+                    );
+                },
+                logout: async () => {
+                    await signOut(getAuth());
+                    setUser(undefined);
+                },
+            }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
