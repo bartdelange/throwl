308d80cdcdfc1f4baaac92dae5e576e05228a4f0
Bart de Lange <bart.de.lange32@gmail.com>
2025-12-29 12:27:36 +0100

    chore: Upgraded to RN 0.83.0

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,28 @@
+/** @type {import('react-native-worklets/plugin').PluginOptions} */
+const workletsPluginOptions = {
+    // Your custom options.
+};
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['.'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '~': './src',
-          '#': './src/constants',
-        },
-      },
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['.'],
+                extensions: [
+                    '.ios.js',
+                    '.android.js',
+                    '.js',
+                    '.ts',
+                    '.tsx',
+                    '.json',
+                ],
+                alias: {
+                    '~': './src',
+                    '#': './src/constants',
+                },
+            },
+        ],
+        ['react-native-worklets/plugin', workletsPluginOptions],
     ],
-    'react-native-reanimated/plugin',
-  ],
 };
--- src/components/ClickableDartboard/ClickableDartboard.tsx
+++ src/components/ClickableDartboard/ClickableDartboard.tsx
@@ -1,73 +1,74 @@
 import React from 'react';
 import interpolateColor from 'color-interpolate';
-import Svg, { Path, Rect } from 'react-native-svg';
-import { DartboardScoreType, Throw } from '~/models/throw';
+import Svg, { Path } from 'react-native-svg';
+import { Throw } from '~/models/throw';
 import { numberParts } from './constants/numbers';
 
 import { parts } from './constants/parts';
 
 export interface ClickablePart extends Throw {
-  svgPath: string;
-  color: string;
+    svgPath: string;
+    color: string;
 }
 
 interface ClickableDartboardProps {
-  onClick?: (score: Throw, pagePos: { x: number; y: number }) => void;
-  heatmap?: Map<string, number>;
+    onClick?: (score: Throw, pagePos: { x: number; y: number }) => void;
+    heatmap?: Map<string, number>;
 }
 
 export const ClickableDartboard: React.FC<ClickableDartboardProps> = ({
-  onClick,
-  heatmap,
+    onClick,
+    heatmap,
 }) => {
-  const [currentlySelected, setCurrentlySelected] =
-    React.useState<ClickablePart>();
+    const [currentlySelected, setCurrentlySelected] =
+        React.useState<ClickablePart>();
 
-  return (
-    <Svg viewBox="0 0 500 500" preserveAspectRatio="none">
-      {numberParts.map((part, i) => (
-        <Path key={i} d={part.svgPath} fill={part.color} />
-      ))}
-      {parts.map((part, i) => {
-        let partColor = part.color;
-        if (heatmap && heatmap.has(`${part.type}/${part.score}`)) {
-          const percentage = heatmap.get(`${part.type}/${part.score}`) || 0;
-          const colorMap = interpolateColor(['#80deea', '#0d47a1']);
-          partColor = colorMap(percentage);
-        }
+    return (
+        <Svg viewBox="0 0 500 500" preserveAspectRatio="none">
+            {numberParts.map((part, i) => (
+                <Path key={i} d={part.svgPath} fill={part.color} />
+            ))}
+            {parts.map((part, i) => {
+                let partColor = part.color;
+                if (heatmap && heatmap.has(`${part.type}/${part.score}`)) {
+                    const percentage =
+                        heatmap.get(`${part.type}/${part.score}`) || 0;
+                    const colorMap = interpolateColor(['#80deea', '#0d47a1']);
+                    partColor = colorMap(percentage);
+                }
 
-        return (
-          <Path
-            key={`${i}-${part.type}-${part.score}`}
-            d={part.svgPath}
-            fill={partColor}
-            onPressIn={() => {
-              setCurrentlySelected(part);
-            }}
-            onPressOut={evt => {
-              if (currentlySelected) {
-                const { svgPath, color, ...clickRest } = currentlySelected;
-                onClick &&
-                  onClick(clickRest, {
-                    x: evt.nativeEvent.pageX,
-                    y: evt.nativeEvent.pageY,
-                  });
-                setCurrentlySelected(undefined);
-              }
-            }}
-          />
-        );
-      })}
-      {currentlySelected && (
-        <Path
-          d={currentlySelected.svgPath}
-          stroke={currentlySelected.color}
-          fill={currentlySelected.color}
-          strokeWidth={20}
-          strokeLinejoin="round"
-          strokeLinecap="round"
-        />
-      )}
-    </Svg>
-  );
+                return (
+                    <Path
+                        key={`${i}-${part.type}-${part.score}`}
+                        d={part.svgPath}
+                        fill={partColor}
+                        onPressIn={() => {
+                            setCurrentlySelected(part);
+                        }}
+                        onPressOut={evt => {
+                            if (currentlySelected) {
+                                const { svgPath, color, ...clickRest } = part;
+                                onClick &&
+                                    onClick(clickRest, {
+                                        x: evt.nativeEvent.pageX,
+                                        y: evt.nativeEvent.pageY,
+                                    });
+                                setCurrentlySelected(undefined);
+                            }
+                        }}
+                    />
+                );
+            })}
+            {currentlySelected && (
+                <Path
+                    d={currentlySelected.svgPath}
+                    stroke={currentlySelected.color}
+                    fill={currentlySelected.color}
+                    strokeWidth={20}
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                />
+            )}
+        </Svg>
+    );
 };
