4bd6b6f9ebabe2057d01ce7b96d52a39cb2316a2
Bart de Lange <bart.de.lange32@gmail.com>
2025-12-29 12:27:36 +0100

    chore: Upgraded to RN 0.83.0

--- src/screens/PlayGame/styles.ts
+++ src/screens/PlayGame/styles.ts
@@ -6,6 +6,7 @@ export const makeStyles = () => {
     const { width, height } = Dimensions.get('window');
 
     const textSize = Math.max(width * 0.05, 30);
+    const dartboardWidth = width < 500 ? '100%' : width * 0.8;
 
     return StyleSheet.create({
         layout: {
@@ -14,12 +15,13 @@ export const makeStyles = () => {
             alignItems: 'center',
         },
         backButton: { position: 'absolute', top: 0, left: 0 },
-        content: { flex: 1 },
+        content: { flex: 1, width: '100%' },
         dartboardWrapper: {
             padding: width * 0.025,
             aspectRatio: 1,
-            width: '100%',
+            width: dartboardWidth,
             position: 'relative',
+            alignSelf: 'center',
         },
         missText: {
             position: 'absolute',
@@ -44,94 +46,12 @@ export const makeStyles = () => {
             borderTopStartRadius: 25,
             borderTopEndRadius: 25,
             alignItems: 'center',
-        },
-        currentTurnScoreContainer: {
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingVertical: height * 0.015,
-            width: width * 0.8,
-        },
-        currentTurnScoreActiveThrow: {},
-        currentThrowContainer: {
-            flexDirection: 'row',
-            flex: 1,
-        },
-        currentThrowNumberText: {
-            fontSize: textSize * 0.5,
-        },
-        currentThrowNumberTextSuperScript: {
-            fontSize: textSize * 0.8 * 0.4,
-        },
-        currentThrowScoreText: {
-            paddingLeft: width * 0.02,
-            fontSize: textSize,
-            ...Platform.select({
-                default: {
-                    fontWeight: 'bold',
-                },
-                android: {
-                    fontFamily: 'Karbon-Bold',
-                },
-            }),
-        },
-        neededScoreText: {
-            opacity: 0.5,
-            fontSize: textSize * 0.5,
+            // minHeight: scoreWrapperHeight,
         },
         undoButton: {
             transform: [{ rotateZ: '45deg' }],
             backgroundColor: colors.onSurface,
             color: colors.primary,
-        },
-        scoreTable: {
-            flex: 1,
-            width: '100%',
-            backgroundColor: colors.primary,
-        },
-        scoreTableHead: {
-            borderTopWidth: 1,
-            borderTopColor: 'white',
-            borderBottomWidth: 1,
-            borderBottomColor: 'white',
-            width: '90%',
-            alignSelf: 'center',
-            height: textSize * 1.25,
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        scoreTableRow: {
-            height: textSize * 1.25,
-            alignItems: 'center',
-            position: 'relative',
-            paddingLeft: width * 0.05,
-            backgroundColor: colors.primary,
-        },
-        scoreTableCol: {
-            flexDirection: 'row',
-        },
-        scoreTableCell: {
-            fontSize: textSize * 0.5,
-            flex: 1,
-        },
-        scoreTableBoldCell: {
-            ...Platform.select({
-                default: {
-                    fontWeight: 'bold',
-                },
-                android: {
-                    fontFamily: 'Karbon-Bold',
-                },
-            }),
-        },
-        scoreTableCenterCell: {
-            textAlign: 'center',
-        },
-        activeUserIconRow: {
-            height: 0,
-        },
-        activeUserIcon: {
-            position: 'absolute',
-            left: width * 0.02,
         },
     });
 };
